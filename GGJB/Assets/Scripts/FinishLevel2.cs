e123c6251d8322e7de0b2b3f91659ad9a79ff32d
fathrdm <114979588+fathrdm@users.noreply.github.com>
2025-01-25 21:53:58 +0800

    commit perpanel lan dan level

---
+++ GGJB/Assets/Scripts/FinishLevel2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel2 : MonoBehaviour
+{
+    public void ContinueUnlockLevel()
+    {
+
+        SceneManager.LoadScene("Level 3");
+
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+
+        }
+
+    }
+}
