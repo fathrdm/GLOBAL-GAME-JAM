714ef2c109d13678c66d816394f292a525906c86
fathrdm <114979588+fathrdm@users.noreply.github.com>
2025-01-26 13:27:56 +0800

    level 1 fix

--- GGJB/Assets/Scripts/FinishLevel1.cs
+++ GGJB/Assets/Scripts/FinishLevel1.cs
@@ -7,28 +7,16 @@ public class FinishPoinPerLevel : MonoBehaviour
 {
     public void ContinueUnlockLevel()
     {
-        string currentLevel = SceneManager.GetActiveScene().name;
 
-        // Periksa apakah ini adalah level terakhir yang telah dicapai
+        SceneManager.LoadScene("Level 2");
+
         if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
         {
             PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
             PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
             PlayerPrefs.Save();
+
         }
 
-        // Tambahkan logika untuk Achievement scene
-        string achievementKey = currentLevel + "_AchievementSeen";
-        if (PlayerPrefs.GetInt(achievementKey, 0) == 0) // Cek apakah Achievement scene belum dilihat
-        {
-            PlayerPrefs.SetInt(achievementKey, 1); // Tandai Achievement scene sebagai sudah dilihat
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("Achievement"); // Arahkan ke scene Achievement
-        }
-        else
-        {
-            // Jika Achievement sudah dilihat, lanjut ke level berikutnya
-            SceneManager.LoadScene("Level 2");
-        }
     }
 }
--- GGJB/Assets/Scripts/LevelMenuManager.cs
+++ GGJB/Assets/Scripts/LevelMenuManager.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.VFX;
 
 public class LevelMenuManager : MonoBehaviour
 {
     public Button[] button;
     private SFXManager sfxmanager;
-
     private void Awake()
     {
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
@@ -26,22 +26,10 @@ public class LevelMenuManager : MonoBehaviour
     {
         sfxmanager = FindObjectOfType<SFXManager>();
     }
-
     public void OpenLevel(int levelId)
     {
         sfxmanager.UIClickSfx();
-
-        string achievementKey = "Level" + levelId + "_AchievementSeen";
-        if (PlayerPrefs.GetInt(achievementKey, 0) == 0) // Cek apakah Achievement scene belum dilihat
-        {
-            PlayerPrefs.SetInt(achievementKey, 1); // Tandai Achievement scene sebagai sudah dilihat
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("Achievement"); // Arahkan ke scene Achievement
-        }
-        else
-        {
-            string levelname = "Level " + levelId;
-            SceneManager.LoadScene(levelname); // Langsung ke level jika Achievement sudah dilihat
-        }
+        string levelname = "Level " + levelId;
+        SceneManager.LoadScene(levelname);
     }
 }
